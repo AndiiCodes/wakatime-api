34bde1e2f97401c9a679bc9f220931f8bc80e2c3
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2024-05-12 18:15:35 +1000

    Update wakeApi.js

--- wakeApi.js
+++ wakeApi.js
@@ -5,36 +5,40 @@ const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
   try {
-    // Dynamic import for node-fetch
+    
     const { default: fetch } = await import('node-fetch');
 
-    // Your WakaTime access token
+   
     const accessToken = process.env.ACCESS_TOKEN;
 
-    // User ID or username for whom you want to fetch statistics
+    
     const user = 'Andiicodes';
 
-    // API endpoint for fetching user's stats
+    
     const url = `https://wakatime.com/api/v1/users/${user}/stats`;
 
-    // Make a GET request with your access token as a header
+    
     const response = await fetch(url, {
       headers: {
         'Authorization': `Bearer ${accessToken}`
       }
     });
 
-    // Check if the request was successful
+   
     if (response.ok) {
-      // Parse the JSON response
+      
       const data = await response.json();
-      // Send the fetched data as JSON response to the client
-      res.json(data);
+
+     
+      const codingHours = data.data.text;
+
+     
+      res.json({ text: codingHours });
     } else {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
   } catch (error) {
-    // Send error response to the client
+    
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
