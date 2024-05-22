b20ea11a1068302be6d29e13975386cb40b74a84
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2024-05-22 13:48:11 +1000

    Update wakeApi.js

--- wakeApi.js
+++ wakeApi.js
@@ -5,34 +5,24 @@ const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
   try {
-    
     const { default: fetch } = await import('node-fetch');
-
-    
     const accessToken = process.env.ACCESS_TOKEN;
-
-    
     const user = 'Andiicodes';
-
-   
     const url = `https://wakatime.com/api/v1/users/${user}/stats`;
 
-  
     const response = await fetch(url, {
       headers: {
         'Authorization': `Bearer ${accessToken}`
       }
     });
 
-   
     if (response.ok) {
       const data = await response.json();
-      
-      res.json(data); 
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      res.json(data);
+    } else {
+      res.status(response.status).json({ error: `HTTP error! Status: ${response.status}` });
     }
   } catch (error) {
-   
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
