a51b2898959778184692dde15e877a2d2a44eb5f
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2024-05-23 11:05:34 +1000

    Update wakeApi.js

--- wakeApi.js
+++ wakeApi.js
@@ -1,42 +1,38 @@
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-const allowedReferers = ['https://github.com', 'https://shields.io', 'http://localhost:3000', 'https://wakatime-api-production.up.railway.app'];
-
-app.use((req, res, next) => {
-  const referer = req.get('Referer');
-  const host = req.get('Host');
-  console.log('Referer:', referer);  
-  console.log('Host:', host);        
-
-  if (!referer || allowedReferers.some(r => referer.startsWith(r)) || host === 'wakatime-api-production.up.railway.app') {
-    next();
-  } else {
-    res.status(403).send('Access Denied');
-  }
-});
-
 app.get('/', async (req, res) => {
   try {
+    
     const { default: fetch } = await import('node-fetch');
+
+    
     const accessToken = process.env.ACCESS_TOKEN;
+
+    
     const user = 'Andiicodes';
+
+   
     const url = `https://wakatime.com/api/v1/users/${user}/stats`;
 
+  
     const response = await fetch(url, {
       headers: {
         'Authorization': `Bearer ${accessToken}`
       }
     });
 
+   
     if (response.ok) {
       const data = await response.json();
-      res.json(data);
-    } else {
-      res.status(response.status).json({ error: `HTTP error! Status: ${response.status}` });
+      
+      res.json(data.data.human_readable_total_including_other_language); 
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
   } catch (error) {
+   
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
